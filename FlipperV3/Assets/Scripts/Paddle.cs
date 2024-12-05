148fe6e9b442983e5b6a3f0b50b38c1b35eb65d7
VERFAILLIE <olivier.verfaillie59@laposte.net>
2024-12-05 19:14:46 +0100

    bug fix

--- FlipperV3/Assets/Scripts/Paddle.cs
+++ FlipperV3/Assets/Scripts/Paddle.cs
@@ -1,17 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-
     public float targetPosition = 75;
     public float originPosition = 0;
+
+    private float paddleUpDuration = 0.15f; // Durée pendant laquelle le paddle reste levé.
 
     public HingeJoint hingeJoint;
     private JointSpring jointSpring;
 
     private KeyCode key;
+
+    private bool isKeyPressed; // Indique si la touche est en cours d'utilisation.
+    private float timer; // Timer pour la durée du paddle levé.
 
     public enum KeyPossibility
     {
@@ -28,21 +30,35 @@ public class Paddle : MonoBehaviour
 
         switch (choiceKey)
         {
-            case KeyPossibility.L1 : key = KeyCode.JoystickButton4; break;
-            case KeyPossibility.R1 : key = KeyCode.JoystickButton5; break;
+            case KeyPossibility.L1: key = KeyCode.JoystickButton4; break;
+            case KeyPossibility.R1: key = KeyCode.JoystickButton5; break;
         }
+
+        isKeyPressed = false;
+        timer = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(key))
+        // Si la touche est pressée et que le paddle n'est pas déjà activé
+        if (Input.GetKeyDown(key) && !isKeyPressed)
         {
+            isKeyPressed = true;
             jointSpring.targetPosition = targetPosition;
+            timer = paddleUpDuration; // Démarrer le timer.
         }
-        else
+
+        // Compte à rebours pour ramener le paddle à l'origine après la durée définie
+        if (isKeyPressed)
         {
-            jointSpring.targetPosition = originPosition;
+            timer -= Time.deltaTime;
+
+            if (timer <= 0f)
+            {
+                jointSpring.targetPosition = originPosition;
+                isKeyPressed = false; // Réinitialiser pour permettre une nouvelle pression.
+            }
         }
 
         hingeJoint.spring = jointSpring;

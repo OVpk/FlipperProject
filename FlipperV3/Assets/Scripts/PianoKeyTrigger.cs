07315e34e7b2d72a25c25e3595c63adc76143063
VERFAILLIE <olivier.verfaillie59@laposte.net>
2024-12-12 16:19:01 +0100

    piano + pool

---
+++ FlipperV3/Assets/Scripts/PianoKeyTrigger.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoKeyTrigger : MonoBehaviour
+{
+    private Vector3 originPosition;
+    private void Start()
+    {
+        originPosition = transform.parent.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        transform.parent.transform.position += new Vector3(0, 0, 0.1f);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        transform.parent.transform.position = originPosition;
+    }
+}
---
+++ FlipperV3/Assets/Scripts/Propulsor.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propulsor : MonoBehaviour
+{
+
+    public Vector3 angle;
+    public float strength;
+
+    public Animation animation;
+
+    public GameObject spawnPoint;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        other.gameObject.transform.position = spawnPoint.transform.position;
+        other.gameObject.GetComponent<Rigidbody>().AddForce(angle * strength);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animation.Play("ballPool");
+    }
+}
